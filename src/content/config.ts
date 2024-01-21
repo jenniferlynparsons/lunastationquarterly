bf31121de1037e7c1a035b4db48da43912d24c49
Jennifer Lyn Parsons <1587316+jenniferlynparsons@users.noreply.github.com>
2024-01-21 16:49:42 -0500

    start work on tailwind integration and use

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,8 +8,13 @@ const blog = defineCollection({
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		heroAltText: z.string().optional(),
+		excerpt: z.string().optional(),
+		author: z.string().optional(), 
+		authorSlug: z.string().optional(),
+		postCategory: z.string().optional(),
+		categorySlug: z.string().optional(),
 	}),
 });
 
@@ -18,9 +23,12 @@ const issues = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		description: z.string().optional(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
+		stories: z.array(z.string()),
+		authors: z.array(z.string()),
+		authorSlugs: z.array(z.string()).optional(),
 	}),
 });
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		container: {
+			center: true,
+		},
+		fontFamily: {
+			sans: ['Oswald', 'sans-serif'],
+			serif: ['Libre Baskerville', 'serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
